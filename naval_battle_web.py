1caef01824a947b47154cb017af4a09e06a8c68b
PaulaS06 <menesesgasca@outlook.com>
2024-11-05 21:46:32 -0500

    Exceptions and changes in "Buacar" and "Eliminar"

--- naval_battle_web.py
+++ naval_battle_web.py
@@ -4,15 +4,9 @@ from src.controller.NavalBattleController import Controller_NB
 from src.model.logic import NavalBattle_logic
 from src.model.NavalBattleModel import Model_NB
 
-def validar_numero(valor):
-    """Función que verifica si el valor es un número."""
-    if not valor.isdigit():
-        raise NavalBattle_logic.NonNumericValueError("** Error: Se ingresó un valor no numérico.")
-    if len(valor) != 5:
-        raise NavalBattle_logic.InvalidStartingCodeError("** Error: El código de partida debe ser un número de exactamente 5 dígitos.")
-
 naval_battle_web = Flask(__name__)
 naval_battle_web.secret_key = b'\xf0\xd1\x06\xb5_\x88\xec\x12=\x03b\xe75U\xd8\xa5'
+
 
 class RouteApp:
 
@@ -25,30 +19,42 @@ class RouteApp:
         return render_template('menu.html')
 
 
+#Post method: Usado para enviar datos al servidor para ser procesados.
+#Get method: Usado para solicitar datos del servidor.
+
+
     @naval_battle_web.route('/buscar', methods=['GET', 'POST'])
     def buscar_partida():
         if request.method == 'POST':
             starting_code = request.form['starting_code']
-            session['starting_code'] = starting_code
-            return redirect(url_for('reporte'))
+            return redirect(url_for('reporte', starting_code=starting_code))
         return render_template('buscar_partida.html')
 
-    @naval_battle_web.route('/reporte', methods=['GET'])
-    def reporte():
-        starting_code = session.get('starting_code')
-        validar_numero(starting_code)
+    @naval_battle_web.route('/reporte')
+    def resultado_buscar():
+        starting_code = request.args.get('starting_code')
 
         if not starting_code:
-            return "Error: starting_code no proporcionado"
+            result_message = "Error: código no proporcionado"
+            return render_template('anuncio_buscar.html', result_message=result_message)
+        if not starting_code.isdigit():
+            result_message = "Error: Debe ingresar un valor numérico."
+            return render_template('anuncio_buscar.html', result_message=result_message)
+        if len(starting_code) != 5:
+            result_message = "Error: El código de partida debe ser un número de exactamente 5 dígitos."
+            return render_template('anuncio_buscar.html', result_message=result_message)
+        
         try:
             partida_buscada = Controller_NB.BuscarCodigoPartida(starting_code)
             if partida_buscada is None: 
-                return "Error: No se encontró el registro con el código proporcionado"
-        except Exception as err:
-            print(f"** Error: No se encontró la partida con código {starting_code}.")
-            raise err
+                result_message = f"Error: No se encontró el registro con el código {starting_code}."
+                return render_template('anuncio_buscar.html', result_message=result_message)
+        
+        except Exception:
+            result_message = f"Error: No se encontró la partida con código {starting_code}, asegúrese de que la partida exista."
+            return render_template('anuncio_buscar.html', result_message=result_message)
 
-        return render_template('reporte_partida.html', 
+        return render_template('resultado_partida.html', 
                             partida=partida_buscada.starting_code,
                             tablero=partida_buscada.rows + "x" + partida_buscada.columns,
                             disparos_efectivos = partida_buscada.hits,
@@ -58,44 +64,55 @@ class RouteApp:
                             puntaje=partida_buscada.score)
 
 
+
     @naval_battle_web.route('/eliminar', methods=['GET', 'POST'])
     def eliminar_partida():
         if request.method == 'POST':
             starting_code = request.form['starting_code']
-            session['starting_code'] = starting_code
-            return redirect(url_for('resultado_eliminacion'))
+            return redirect(url_for('resultado_eliminacion', starting_code=starting_code))
         return render_template('eliminar_partida.html')
     
     @naval_battle_web.route('/anuncio_eliminar', methods=['GET'])
     def resultado_eliminacion():
-        starting_code = session.get('starting_code')
-        validar_numero(starting_code)
+        starting_code = request.args.get('starting_code')
         
         if not starting_code:
-            return "Error: starting_code no proporcionado"
+            result_message = "Error: código no proporcionado"
+            return render_template('anuncio_eliminar.html', result_message=result_message)
+        if not starting_code.isdigit():
+            result_message = "Error: Debe ingresar un valor numérico."
+            return render_template('anuncio_eliminar.html', result_message=result_message)
+        if len(starting_code) != 5:
+            result_message = "Error: El código de partida debe ser un número de exactamente 5 dígitos."
+            return render_template('anuncio_eliminar.html', result_message=result_message)
         
         try:
-            validar_numero(starting_code)
             Controller_NB.EliminarPartida(starting_code)
             result_message = f"Se eliminó la partida con código {starting_code} exitosamente."
-        except Exception as err:
+            return render_template('anuncio_eliminar.html', result_message=result_message)
+        except Exception:
             result_message = f"Error: No se encontró la partida con código {starting_code}."
-            print(err)  # Log the error for debugging purposes
+            return render_template('anuncio_eliminar.html', result_message=result_message)
         
-        return render_template('anuncio_eliminar.html', 
-                               partida=starting_code, 
-                               result_message=result_message)
         
+
     @naval_battle_web.route('/insertar', methods=['GET', 'POST'])
     def insertar_partida():
         if request.method == 'POST':
             try:
-                # Obtener los valores del formulario
+                
                 starting_code = request.form.get('starting_code')
-                validar_numero(starting_code)
+
+                if not starting_code:
+                    result_message = "Error: código no proporcionado"
+                    return render_template('anuncio_insertar.html', result_message=result_message)
+                if not starting_code.isdigit():
+                    result_message = "Error: Debe ingresar un valor numérico."
+                    return render_template('anuncio_insertar.html', result_message=result_message)
                 if len(starting_code) != 5:
-                    raise NavalBattle_logic.InvalidStartingCodeError("** Error: El código de partida debe ser un número de exactamente 5 dígitos.")
-    
+                    result_message = "Error: El código de partida debe ser un número de exactamente 5 dígitos."
+                    return render_template('anuncio_insertar.html', result_message=result_message)
+
                 rows = request.form.get('rows')
                 rows = int(rows)
                 columns = request.form.get('columns')
@@ -120,19 +137,21 @@ class RouteApp:
                     max_possible_shots=max_possible_shots,
                     score=score
                 )
-                # Guardar en la base de datos
+
                 Controller_NB.Insertar(nueva_partida)
-                flash('Partida insertada exitosamente', 'success')
-                return redirect(url_for('anuncio_insertar'))
-            except Exception as e:
-                flash(str(e), 'error')
-                return redirect(url_for('anuncio_insertar'))
+                result_message = f'Partida {starting_code} insertada exitosamente'
+                return render_template('anuncio_insertar.html')
+            
+            except Exception:
+                result_message = f'Error al insertar la partida, intentelo de nuevo'
+                return render_template('anuncio_insertar.html', result_message=result_message)
         return render_template('insertar_partida.html')
     
-    @naval_battle_web.route('/anuncio_insertar', methods=['GET'])
+    @naval_battle_web.route('/anuncio_insertar')
     def anuncio_insertar():
         return render_template('anuncio_insertar.html')
 
     
+
 if __name__ == '__main__':
     naval_battle_web.run(debug=True)
